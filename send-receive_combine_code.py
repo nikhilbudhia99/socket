d5f93b8603258a945023a8eb89bf086c136827bf
root <nikhilbudhia99@yahoo.com>
2018-06-04 17:14:15 +0530

    socket code 1

---
+++ send-receive_combine_code.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+#senders code
+import socket
+
+s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+for i in range(5):
+        msg=raw_input("enter your message :  ")
+        s.sendto(msg,("192.168.122.1",9876))
+
+#receivers code
+#!/usr/bin/python2
+
+import socket
+
+s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+s.bind(('192.168.122.1',9876))
+
+for i in range(5):
+	print s.recvfrom(6)
+
+
+
+~                                                                               
+~                                       
